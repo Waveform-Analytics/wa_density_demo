445970823ee75f3f6fb85a34459c0989eb394868
Michelle Weirathmueller <1017259+michellejw@users.noreply.github.com>
2024-03-13 11:20:02 -0400

    add requirements.txt

--- prep_density_data.py
+++ prep_density_data.py
@@ -1,3 +1,30 @@
 """
-
+In this script, we're preparing the density data for loading into an Observable
+notebook. To simplify things, we're just going to find the density *.img files and
+export them to simple csv files.
 """
+
+import rasterio
+from rasterio.enums import Resampling
+import os
+import glob
+
+# First, we'll find all of the data files
+density_folder = "data/density"
+glob.glob(density_folder)
+
+# Open the .img file
+with rasterio.open('path_to_your_file.img') as src:
+    # Read the data
+    data = src.read()
+
+    # Copy the metadata
+    meta = src.meta.copy()
+
+    # Update the metadata for GeoTIFF
+    meta.update(driver='GTiff')
+
+    # Write to a new GeoTIFF file
+    with rasterio.open('output_filename.tif', 'w', **meta) as dst:
+        dst.write(data)
+
